1caa01febd96c36b48d56ff0d8062fe67de59857
stephaniehohenberg <stephanie.hohenberg@gmail.com>
2025-05-01 17:47:59 -0600

    fixes css mobile view error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
 import {Component, OnDestroy} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {AuthenticationService} from './services/authentication.service';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnDestroy {
 
   constructor(private auth: AuthenticationService) {}
+
+  public isBigScreen() {
+    if (typeof window === 'undefined') {
+      return false;
+    }
+    return window.innerWidth >= 420;
+  }
 
   public ngOnDestroy() {
     this.auth.logOut();
